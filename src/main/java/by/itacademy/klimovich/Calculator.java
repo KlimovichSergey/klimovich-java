580a8f204c4eee83e671088c4eb3050601577697
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-07-30 21:42:41 +0300

    add interfeca& class Calculator

--- src/main/java/by/itacademy/klimovich/Calculator.java
+++ src/main/java/by/itacademy/klimovich/Calculator.java
@@ -1,4 +1,24 @@
 package by.itacademy.klimovich;
 
-public class Calculator {
+public class Calculator implements ICalculator {
+    @Override
+    public int sum(int a, int b) {
+
+        return a + b;
+    }
+
+    @Override
+    public int multiplay(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public double divide(int a, int b) {
+        return 0;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return 0;
+    }
 }
---
+++ src/main/java/by/itacademy/klimovich/ICalculator.java
@@ -0,0 +1,8 @@
+package by.itacademy.klimovich;
+
+public interface ICalculator {
+   int sum(int a, int b);
+   int multiplay(int a, int b);
+   double divide(int a, int b);
+   int subtract(int a, int b);
+}
