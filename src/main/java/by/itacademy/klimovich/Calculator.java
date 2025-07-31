ec3c53adf21f31d089d16fceb4cc77e9932210eb
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-07-31 23:17:03 +0300

    add junit     Exception in method

--- src/main/java/by/itacademy/klimovich/Calculator.java
+++ src/main/java/by/itacademy/klimovich/Calculator.java
@@ -2,23 +2,37 @@ package by.itacademy.klimovich;
 
 public class Calculator implements ICalculator {
     @Override
-    public int sum(int a, int b) {
-
-        return a + b;
+    public int sum(int a, int b) throws IndexOutOfBoundsException {
+        int result = a + b;
+        if (result > Integer.MAX_VALUE) {
+            throw new IndexOutOfBoundsException("Result greater that maximum int");
+        }
+        return result;
     }
 
     @Override
-    public int multiplay(int a, int b) {
-        return 0;
+    public int multiplay(int a, int b) throws IndexOutOfBoundsException {
+        int result = a * b;
+        if (result > Integer.MAX_VALUE) {
+            throw new IndexOutOfBoundsException("Result greater that maximum int");
+        }
+        return result;
     }
 
     @Override
-    public double divide(int a, int b) {
-        return 0;
+    public double divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
     }
 
     @Override
-    public int subtract(int a, int b) {
-        return 0;
+    public int subtract(int a, int b) throws IndexOutOfBoundsException {
+        int result = a - b;
+        if (result < Integer.MIN_VALUE) {
+            throw new IndexOutOfBoundsException("Result greater that minimum int");
+        }
+        return result;
     }
 }
---
+++ src/main/java/by/itacademy/klimovich/Main.java
@@ -0,0 +1,17 @@
+package by.itacademy.klimovich;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        try {
+            System.out.println("Сложение: " + calculator.sum(10,20));
+            System.out.println("Вычетание: " + calculator.subtract(10,20));
+            System.out.println("Умножение: " + calculator.multiplay(10,200));
+            System.out.println("Деление: " + calculator.divide(10,0));
+        } catch (ArithmeticException  | IndexOutOfBoundsException e) {
+            System.err.println("Перехвачино исключение" + e);
+
+        }
+    }
+}
