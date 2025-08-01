e0a7769b04498fb9dce58598f60f8d6b8cf9fb0d
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-08-02 01:49:11 +0300

    refactor JUNIT add test method

--- src/test/java/by/itacademy/klimovich/CalculatorTest.java
+++ src/test/java/by/itacademy/klimovich/CalculatorTest.java
@@ -1,4 +1,75 @@
 package by.itacademy.klimovich;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    public void calculator_create_initialize() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSum() {
+        int actual = calculator.sum(10, 20);
+        assertEquals(30, 30, "10 + 20 равно 30");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-20, -10, -30",
+            "20, -10, 10",
+            "20,-20, 0"
+    })
+    @DisplayName("Проверка метода сложения разными аргументами")
+    public void testSum(int a, int b, int actual) {
+        assertEquals(actual, calculator.sum(a, b));
+    }
+
+    @Test
+    public void testSubtract() {
+        int actual = calculator.sum(20, 10);
+        assertEquals(actual, 30, "20 - 10 равно 10");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-25, -10, -15",
+            "20, -10, 30",
+            "20,-20, 0",
+            "-20, 0, -20"
+    })
+    @DisplayName("Проверка метода вычетание разными аргументами")
+    public void testSubtract(int a, int b, int actual) {
+        assertEquals(actual, calculator.subtract(a, b));
+    }
+
+    @Test
+    public void testDivide() {
+        double actual = calculator.divide(10, 2);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    public void testDivideException() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(10, 0);
+        });
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+
+    @Test
+    public void testMultiplay() {
+        int actual = calculator.multiplay(10, 6);
+        assertEquals(60, actual);
+    }
 }
