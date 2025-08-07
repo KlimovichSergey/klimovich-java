bcd02bcebcc13299c35ca064b7b07832b3452443
Sergei Klimovich <klimovichsergey122@gmail.com>
2025-08-07 23:42:42 +0300

    add package pizzatempo             dodopizza             HomePageLocator.java

---
+++ src/main/java/by/dodopizza/HomePageLocator.java
@@ -0,0 +1,5 @@
+public static final String DIV_TITLE = "//div[@class='\"sc-135lane-0 jusXuE\"]/class=\"title\"']";
+public static final String SPAN_LABEL_LEFT = "//span[@class ='label  left']";
+public static final String DIV_INPUT_SUB_CONTAINER ="//div[@class='input-sub-container']";
+public static final String BUTTON_LOGIN_SUBMIT = "//button[@data-testid='\"login_submit_button\"']";
+public static final String BUTTON_POPUP_CLOSE = "//button[@class=\"popup-close-button\"']";
---
+++ src/main/java/by/pizzatempo/LoginPageLacator.java
@@ -0,0 +1,6 @@
+public static final String TITLE = "//form[@method='post']/div/class='title';
+public static final String ASTROAUTH_login_MAIL ="//input[@name='astroauth_login']";
+public static final String PASSWORD_TEXT_ASTROAUTH ="//input[@name='astroauth_pass']";
+public static final String SEARCHBUTTOM = "//input[@name='astroauth_submit'];
+public static final String TEXT_REGISTRATION ="//a[@href='\"https://www.pizzatempo.by/registration/\"']";
+public static final String TEXT_FOGOT_REGISTRATION = '//a[@href='"https://www.pizzatempo.by/registration/forgot/']";
